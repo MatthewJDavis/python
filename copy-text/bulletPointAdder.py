5a67722ba511ca5685f21a60d582a52f8fc7792a
= <matthewdavis111@gmail.com>
2018-09-10 19:27:31 -0400

    added requirments and bulletpoint files

---
+++ copy-text/bulletPointAdder.py
@@ -0,0 +1,12 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start of each line on the clipboard
+
+import pyperclip
+text = pyperclip.paste()
+
+#TODO: Separate lines and add stars.
+lines = text.split('\n')
+for i in range(len(lines)):
+    lines[i] = '*' + lines[i]
+text = '\n'.join(lines)
+pyperclip.copy(text)
