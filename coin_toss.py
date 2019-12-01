ad08000fba0610a794f432f57d8557e1b5fa37ae
MatthewJDavis <matthewdavis111@gmail.com>
2019-12-01 02:41:48 -0500

    added most in a row on one side func

--- coin_toss.py
+++ coin_toss.py
@@ -24,15 +24,47 @@ def most_in_a_row(item_list):
         else:
             current_item = item
             count = 1
-    return current_item, highest_count
+    return highest_count
+
+
+    
+def most_one_side_in_a_row(item_list):
+    """ Calculate the most times each side appears in a list contiguously"""
+    heads_count = 0
+    tails_count = 0
+    max_heads_count = 0
+    max_tails_count = 0
+    current_item = ''
+    count = 0
+    for item in item_list:
+        current_item = item
+        count += 1
+        if current_item == 'Heads':
+            heads_count += count
+            if heads_count > max_heads_count:
+                max_heads_count = heads_count
+                tails_count = 0
+                count = 0
+            count = 0
+            tails_count = 0
+        else:
+            tails_count += count
+            if tails_count > max_tails_count:
+                max_tails_count = tails_count
+                heads_count = 0
+                count = 0
+            count = 0
+            heads_count = 0
+
+    return max_heads_count, max_tails_count
 
 if __name__ == '__main__':
     outcome = []
-    for i in range (1000):
+    for i in range (100000):
         outcome.append(coin_toss())
     print(outcome)
-    print(most_in_a_row(outcome))
+    print('Highest in a row: {} '.format(most_in_a_row(outcome)))
     print('Total Tails {}'.format(outcome.count('Tails')))
     print('Total Heads {}'.format(outcome.count('Heads')))
-
-
+    a = most_one_side_in_a_row(outcome)
+    print('Most Heads in a row: {}\nMost Tails in a row: {} '.format(a[0], a[1]))
