83ff03f0a833eb2441a145a6976734e3259177b2
MatthewJDavis <matthewdavis111@gmail.com>
2020-01-19 11:00:55 -0500

    add main function

--- teamcity_sha.py
+++ teamcity_sha.py
@@ -11,9 +11,18 @@ def get_sha(url):
     return sha_words
 
 
-versionList = {'2019.1.5', '2019.1.4', '2019.1.3', '2019.1.2', '2019.1.1', '2019.1'}
+def print_items(items):
+    for item in items:
+        print(item)
 
-for version in versionList:
-    url = 'https://download.jetbrains.com/teamcity/TeamCity-{}.tar.gz.sha256'.format(version)
-    get_sha(url)
 
+def main():
+    versionList = {'2019.1.5', '2019.1.4', '2019.1.3', '2019.1.2', '2019.1.1', '2019.1'}
+
+    for version in versionList:
+        url = 'https://download.jetbrains.com/teamcity/TeamCity-{}.tar.gz.sha256'.format(version)
+        print_items(get_sha(url))
+
+
+if __name__ == '__main__':
+    main()
