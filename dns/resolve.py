3e7e38392d59b4c22ea170e389eafc19a368a892
MatthewJDavis <matthewdavis111@gmail.com>
2020-02-20 21:14:31 -0500

    update to cmdline arg input

--- dns/resolve.py
+++ dns/resolve.py
@@ -1,8 +1,12 @@
-import dns.resolver
+import dns.resolver, sys
+
+if len(sys.argv) < 2:
+    print('Usage: python resolve.py [domain name]')
+    sys.exit()
 
 resolver = dns.resolver.Resolver()
 
-answers = resolver.query('matthewdavis111.com', 'A')
+answers = resolver.query(sys.argv[1], 'A')
 
 for answer in answers:
     print(answer)
