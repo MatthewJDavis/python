0040ccf1ca56abdf726bb5a1aeb27981c413d9b1
MatthewJDavis <matthewdavis111@gmail.com>
2020-04-02 20:23:50 -0400

    add comment

---
+++ delay_build/delay_build.py
@@ -0,0 +1,14 @@
+import time
+''' Output text one char at a time with a sleep in between for testing builds in CI '''
+
+text = 'this is a bit of a long line of text and would make the linter very very sad :('
+doubled = text * 2
+start = time.time()
+
+for char in text:
+    print(char)
+    time.sleep(1)
+
+finish = time.time()
+
+print(f'Build took {start - finish:2} to complete.')
